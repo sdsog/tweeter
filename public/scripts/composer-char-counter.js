5ccd7f6900e9ed5d8f23a595e9bb5d412379445f
sdsog <34170634+sdsog@users.noreply.github.com>
2019-06-11 21:49:02 +0000

    fixed bug on char counter js after code cleanup

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
-  //sets the maximum character length for input
-  const tweetLength = 140;git a
+  //sets the maximum character length for a tweet 
+  const tweetLength = 140;
   textArea = document.getElementById("tweet-text-input");
   $(textArea).on('keyup', function () {
+    let counter;
     let remainCharCounter;
-    remainCharCounter = $(this).siblings('.counter') - $(this).val().length;
+    counter = $(this).siblings('.counter');
+    remainCharCounter = tweetLength - $(this).val().length;
     counter.text(remainCharCounter);
     if (remainCharCounter <= 0) {
-      counter.addClass("red");
+      counter.addClass('red');
+    } else {
+      counter.removeClass('red');
     }
   });
 });
