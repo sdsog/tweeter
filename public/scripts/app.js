32715dc60e23d4ca946b58b4624936cb282e0f50
sdsog <34170634+sdsog@users.noreply.github.com>
2019-06-12 04:13:34 +0000

    mapped database input to article

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,94 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Test / driver code (temporary). Eventually will get this from the server.
+const data = [{
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
+$(document).ready(function () {
+
+
+  function renderTweets(tweets) {
+
+
+    for (let i of tweets) {
+
+      $tweet = createTweetElement(i);
+
+      $('#tweet-container').prepend($tweet);
+    }
+  }
+
+
+
+  const createTweetElement = function (tweet) {
+    var $article = $('<article>').addClass('tweet-feed');
+    var $header = $('<header>').addClass('tweet-header').appendTo($article);
+    $('<img>').attr('src', tweet.user.avatars.small).addClass('profile-icon').appendTo($header);
+    $('<h2>').text(tweet.user.name).addClass('twitter-name').appendTo($header);
+    $('<span>').text(tweet.user.handle).addClass('handle').appendTo($header);
+    var $section = $('<p>').addClass('tweet-body').appendTo($article);
+    $('<p>').text(tweet.content.text).appendTo($section);
+    var $footer = $('<footer>').addClass('tweet-footer').appendTo($article);
+    $('<span>').text(tweet.created_at).addClass('date').appendTo($footer);
+    $('<img>').addClass('icon').attr('src', 'https://img.icons8.com/material-rounded/24/000000/like.png').appendTo($footer);
+    $('<img>').addClass('icon').attr('src', 'https://img.icons8.com/material/24/000000/refresh.png').appendTo($footer);
+    $('<img>').addClass('icon').attr('src', 'https://img.icons8.com/material/24/000000/filled-flag.png').appendTo($footer);
+
+    return $article;
+  };
+
+
+
+
+
+  // var $tweet = createTweetElement(tweetData);
+
+  // // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+  // $('#tweet-container').append($tweet);
+
+  renderTweets(data);
+
+});
