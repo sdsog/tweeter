19b6f58ea62ad0c4b51cf918456b1e072443cd6a
sdsog <34170634+sdsog@users.noreply.github.com>
2019-06-12 21:51:00 +0000

    added pull from tweet database and populated on page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,51 +1,3 @@
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [{
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
 const timeAgo = (ts) => {
   const d = new Date();
   const nowTs = Math.floor(d.getTime() / 1000);
@@ -71,18 +23,14 @@ const timeAgo = (ts) => {
 
 $(document).ready(function () {
 
-const loadTweets = () = {
-
-  $('#fetch-posts').click(() => {
-    $.getJSON(`/tweets`, (data) => {
-        const $tweets = $('#tweets');
-        $posts.empty();
-        data.slice(5, 15).forEach(createPost);
-    });
-});
-
-
-}
+  const loadTweets = () => {
+    //loads tweets from database
+    $.getJSON("/tweets")
+      // add handler when getJSON is resolved to execute render
+      .done(function (tweets) {
+        renderTweets(tweets);
+      });
+  }
 
   const renderTweets = (tweets) => {
     for (let tweet of tweets) {
@@ -117,5 +65,6 @@ const loadTweets = () = {
     });
   });
 
-  renderTweets(data);
+  loadTweets();
+
 });
