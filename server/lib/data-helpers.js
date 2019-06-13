3d09c8d9140196ad6242fbc1e073e7558bb10476
sdsog <34170634+sdsog@users.noreply.github.com>
2019-06-13 21:06:10 +0000

    connected and setup MongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -8,20 +8,31 @@ module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+    saveTweet: function (newTweet, callback) {
+
+      db.collection("tweets").insertOne(newTweet, (err, result) => {
+        if (err) {
+          return callback(err);
+        }
         callback(null, true);
       });
+
     },
 
     // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      simulateDelay(() => {
+    getTweets: function (callback) {
+
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
+
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+        callback(null, tweets.sort(sortNewestFirst));
+
       });
-    }
+
+    },
 
   };
 }
