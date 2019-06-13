92e988c02e019e6372b518d48297b8460fdde332
sdsog <34170634+sdsog@users.noreply.github.com>
2019-06-13 06:55:22 +0000

    added styling to layout

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
     $data = $textarea.serialize();
 
     if ($textLength > 140) {
-      $message.text("Only 140 characters allowed!").toggle(true);
+      $message.text("Ain't nobody got time for that! Only 140 characters allowed!").toggle(true);
       $textarea.focus();
       console.log("text too long");
     } else if ($text === "" || $text === null) {
